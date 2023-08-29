b2676955c656577478a7059eeae5c9234da81213
lferkal <ferkido777@gmail.com>
2023-08-29 22:21:18 +0500

    Web-Informatika

---
+++ js.js
@@ -0,0 +1,58 @@
+// animation-scroll \\
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
+
+// loader \\
+
+
+
+window.addEventListener("DOMContentLoaded", () =>{
+    const loader = document.querySelector(".spinner-wrapper");
+    setTimeout(function () {
+        loader.style.opacity = "0";
+        setTimeout(function (){
+            loader.style.display = "none";
+        },2000);
+    },6000);  
+});
